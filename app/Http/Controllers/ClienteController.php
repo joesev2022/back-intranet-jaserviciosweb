a733193a2d55b3f20d2e59774b687366d54f3b0f
Jose_Apaza <jose.apazasev@gmail.com>
2023-01-24 10:35:43 -0500

    First commit - Clientes Module

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Cliente;
+
+class ClienteController extends Controller
+{
+    //Funcion para obtener clientes
+    public function get_all_clientes(){
+        //Obtengo a los clientes desde la BD
+        $clientes = Cliente::all();
+        //Retorno una respuesta json con los clientes y con código 200
+        return response()->json([
+            'clientes' => $clientes
+        ], 200);
+    }
+
+    //Funcion para buscar clientes
+    public function search_clientes(Request $request){
+        $search = $request->get('s');
+        if($search != null){
+            $clientes = Cliente::where('nombre', 'LIKE', "%$search%")->get();
+            return response()->json([
+                'clientes' => $clientes
+            ], 200); 
+        } else {
+            return $this->get_all_clientes();
+        }
+    }
+
+    //Funcion para registrar clientes
+    public function store_cliente(Request $request) {
+        $cliente = new Cliente();
+        $cliente->nombre = $request->nombre;
+        $cliente->apellido = $request->apellido;
+        $cliente->celular = $request->celular;
+        $cliente->email = $request->email;
+        $cliente->direccion = $request->direccion;
+        $cliente->descripcion = $request->descripcion;
+        $cliente->save();
+        return response()->json([
+            'cliente' => $cliente,
+            'mensaje' => 'Cliente registrado correctamente.'
+        ], 200); 
+    }
+
+    public function edit_cliente($id) {
+        $cliente = Cliente::findOrFail($id);
+        return response()->json([
+            'cliente' => $cliente
+        ], 200); 
+    }
+
+    public function update_cliente(Request $request, $id) {
+        $cliente = Cliente::findOrFail($id);
+        $cliente->nombre = $request->nombre;
+        $cliente->apellido = $request->apellido;
+        $cliente->celular = $request->celular;
+        $cliente->email = $request->email;
+        $cliente->direccion = $request->direccion;
+        $cliente->descripcion = $request->descripcion;
+        $cliente->update($request->all());
+        return response()->json([
+            'cliente' => $cliente,
+            'mensaje' => 'Cliente actualizado correctamente.'
+        ], 200); 
+    }
+
+    public function delete_cliente($id) {
+        $cliente = Cliente::findOrFail($id);
+        $cliente->delete();
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+use App\Http\Controllers\ClienteController;
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/get_all_clientes', [ClienteController::class, 'get_all_clientes']);
+Route::get('/search_clientes', [ClienteController::class, 'search_clientes']);
+Route::post('/store_cliente', [ClienteController::class, 'store_cliente']);
+Route::post('/update_cliente/{id}', [ClienteController::class, 'update_cliente']);
+Route::get('/edit_cliente/{id}', [ClienteController::class, 'edit_cliente']);
+Route::get('/delete_cliente/{id}', [ClienteController::class, 'delete_cliente']);
